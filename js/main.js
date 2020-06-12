c252033d5cd26f806d3d7647ba093769d437aef4
izumi <kizumi916@gmail.com>
2020-06-12 15:26:30 +0900

    スタイル

--- js/main.js
+++ js/main.js
@@ -22,38 +22,48 @@
     }, 10);
   }
 
-  function setButtonStateInitial(){
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = true;
-
-  }
-  function setButtonStateRunning(){
-    start.disabled = true;
-    stop.disabled = false;
-    reset.disabled = true;
-  }
-  function setButtonStateStoped(){
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = false;
+  function setButtonStateInitial() {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.add('inactive');
   }
 
-  setButtonStateInitial()
+  function setButtonStateRunning() {
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
+  }
+
+  function setButtonStateStopped() {
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.remove('inactive');
+  }
+
+  setButtonStateInitial();
 
   start.addEventListener('click', () => {
+    if (start.classList.contains('inactive') === true) {
+      return;
+    }
     setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
-    setButtonStateStoped();
+    if (stop.classList.contains('inactive') === true) {
+      return;
+    }
+    setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    if (reset.classList.contains('inactive') === true) {
+      return;
+    }
     setButtonStateInitial();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
